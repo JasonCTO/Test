76df74a235e060465ec479febee680672a9e6b49
JasonCTO <pmlxlcs@163.com>
2017-12-23 00:22:18 +0800

    DD

---
+++ src/TestC.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : TestC.cpp
+// Author      : Jason
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!11" << endl; // prints !!!Hello World!!!
+	return 0;
+}
